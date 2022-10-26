9ced06a95867163be3a4b50aa6562ffece56bcb8
PaoloCrescenz <crescenzipaolo2@gmail.com>
2022-10-26 17:08:31 +0200

    Primo Javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const userName = prompt("Inserisci il tuo nome:")
+
+const userSurname = prompt("Inserisci il tuo cognome:")
+
+const userColor = prompt("Inserisci il tuo colore preferito:")
+
+const number = 21
+
+const password = document.getElementById("password")
+password.innerHTML = userName + userSurname + userColor + number
